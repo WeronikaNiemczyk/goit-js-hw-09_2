9ba5489a27580a4c06ab1b317a4603260aae562c
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2024-02-08 21:33:21 +0100

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector(`body`);
+console.log(body);
+let timerId;
 
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.getElementsByTagName(
+      'body'
+    )[0].style.backgroundColor = `#${Math.floor(
+      Math.random() * 16777215
+    ).toString(16)}`;
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
