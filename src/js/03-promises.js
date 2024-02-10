89e63da3c3b7b2e032b45c3b4e01f5170ed2de38
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2024-02-10 22:39:47 +0100

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const delayInput = document.querySelector('[name="delay"]');
+const stepInput = document.querySelector('[name="step"]');
+const amountInput = document.querySelector('[name="amount"]');
+const form = document.querySelector('.form');
+
+const createPromise = (position, delay) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+const onSubmit = event => {
+  event.preventDefault();
+  let delayValue = parseInt(delayInput.value);
+  let stepValue = parseInt(stepInput.value);
+  let amountValue = parseInt(amountInput.value);
+  for (let position = 1; position <= amountValue; position++) {
+    createPromise(position, delayValue)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delayValue += stepValue;
   }
-}
+};
+
+form.addEventListener('submit', onSubmit);
